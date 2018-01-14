4a1de2d7a0b757d5ed7e216d711978fc421af832
MelisaJ <MAJohnson4683@muleriders.saumag.edu>
2018-01-14 16:43:29 -0600

    Start Menu up and running

---
+++ FinalGameProjectSpring2018/Assets/scripts/startMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startMenu : MonoBehaviour {
+  
+
+    public void Update()
+    {
+
+    }
+    public void StartButton()
+    {
+        SceneManager.LoadScene("main", LoadSceneMode.Single);//load scene level
+    }
+    public void LoadButton()
+    {
+
+    }
+    public void CreditsButton()
+    {
+        //credits.SetActive(true);
+
+    }
+}
+
+
