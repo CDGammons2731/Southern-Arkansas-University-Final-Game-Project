9fd8a31e886f8ff43f45151174dd7b77105e88e7
HOME\AVLinnear5534 <aaronlinnear@gmail.com>
2018-01-18 16:53:01 -0600

    Gameplay Test Stuff Uploaded: Saving mostly done, simple player movement, simple health system

---
+++ FinalGameProjectSpring2018/Assets/Game Manager (TEST)/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
+using System.IO;
+
+using System;
+using SAVE;
+[Serializable]
+public class GameManager : MonoBehaviour {
+	public PlayerHealth P;
+	public Text stats;
+
+	public SaveGame SG;
+	// @TESTING
+	public void HealthUp(){
+		SG.health += 100;
+	}
+	public void ArmorUp(){
+		SG.armor += 50;
+	}
+	public void ScoreUp(){
+		SG.score += 250;
+	}
+	public void NameChange(){
+
+	}
+
+	void Update(){
+		
+	
+	}
+	//End TESTING
+}
