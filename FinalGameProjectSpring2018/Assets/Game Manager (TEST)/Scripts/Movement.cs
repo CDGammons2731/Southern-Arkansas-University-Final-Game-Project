69316053956d808f39f057a9dbd5bceba89e6b9b
HOME\AVLinnear5534 <aaronlinnear@gmail.com>
2018-01-18 17:16:23 -0600

    Slight movement edits

--- FinalGameProjectSpring2018/Assets/Game Manager (TEST)/Scripts/Movement.cs
+++ FinalGameProjectSpring2018/Assets/Game Manager (TEST)/Scripts/Movement.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-
+    public bool isJumping = false;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update()
-	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKey(KeyCode.Space)) {
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.up * 5.0f, ForceMode.Impulse);
+            isJumping = true;
+        }
+       
 	}
 }
