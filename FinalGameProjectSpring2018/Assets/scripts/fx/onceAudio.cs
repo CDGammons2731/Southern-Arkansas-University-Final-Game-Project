362c9a95594efde0a3aab15052528f7d248a967e
MAIN\mober <moberry16@gmail.com>
2018-01-24 15:44:54 -0600

    added audio prefabs and folders

---
+++ FinalGameProjectSpring2018/Assets/scripts/fx/onceAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onceAudio : MonoBehaviour {
+
+
+	public AudioClip sound;
+
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter (Collider col) {
+		if (col.CompareTag ("Player")) {
+			GetComponent<AudioSource> ().PlayOneShot (sound);
+		}
+	}
+}
