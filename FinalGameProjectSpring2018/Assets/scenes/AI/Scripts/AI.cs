782c61840fc8fc2025ed803c4e859147b27e4092
DESKTOP-RKUIVOR\TheAuthor <Gammons2731@muleriders.saumag.edu>
2018-02-01 11:41:47 -0600

    AI now moves and updates its destination to reach the player.

---
+++ FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour 
+{
+
+	public Transform destination;
+
+	private NavMeshAgent agent;
+
+	void Update () 
+	{
+		agent = gameObject.GetComponent<NavMeshAgent>();
+
+		agent.SetDestination(destination.position);
+	}
+
+}
