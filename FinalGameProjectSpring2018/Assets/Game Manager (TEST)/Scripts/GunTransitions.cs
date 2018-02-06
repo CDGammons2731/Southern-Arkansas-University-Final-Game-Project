554d3efa3d9fdec66ee6e39d1b2c284ba2012e5b
HOME\AVLinnear5534 <aaronlinnear@gmail.com>
2018-02-05 20:13:22 -0600

    Added player scoping. Still plenty of bugs in places but this will do for now

--- FinalGameProjectSpring2018/Assets/Game Manager (TEST)/Scripts/GunTransitions.cs
+++ FinalGameProjectSpring2018/Assets/Game Manager (TEST)/Scripts/GunTransitions.cs
@@ -4,17 +4,55 @@ using UnityEngine;
 
 public class GunTransitions : MonoBehaviour {
 	public Animator animator;
+    public GameObject scopeOverlay;
+    public GameObject weaponCamera;
+    public GameObject crossHair;
+    public Camera playerCam; //for referenced scopping
+    public float scopeFOV = 15f; //field of view to be zoomed in
+    private float normalFOV; //original fov
+
+    
 	private bool Scoped=false;
 	// Use this for initialization
 	void Start () {
-		
+       
+
 	}
 
 	void Update () {
-		if (Input.GetKeyUp(KeyCode.LeftShift)) {
-			Scoped = !Scoped;
-			animator.SetBool ("isScoped",Scoped);
-		}
+        if (Input.GetMouseButtonDown(1))
+   
+        {
+            
+                Scoped = !Scoped;
+                animator.SetBool("isScoped", Scoped);
+                if (Scoped)
+                    StartCoroutine(OnScoped());
+                else
+                    OnUnscoped();
+
+            
+        }
 
 	}
+
+    IEnumerator OnScoped() {
+
+        yield return new WaitForSeconds(0.15f);
+        scopeOverlay.SetActive(true);
+        weaponCamera.SetActive(false);
+        crossHair.SetActive(false);
+
+        normalFOV = playerCam.fieldOfView;
+        playerCam.fieldOfView = scopeFOV;
+        
+    }
+
+    void OnUnscoped() {
+        scopeOverlay.SetActive(false);
+        weaponCamera.SetActive(true);
+        crossHair.SetActive(true);
+
+        playerCam.fieldOfView = normalFOV;
+    }
 }
