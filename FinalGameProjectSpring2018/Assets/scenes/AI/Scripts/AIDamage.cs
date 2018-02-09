a430846cdff4d365609ae861adcb0b8f2a03af6b
DESKTOP-RKUIVOR\TheAuthor <Gammons2731@muleriders.saumag.edu>
2018-02-09 14:47:47 -0600

    Copied Arrons scene to begin constructing and implementing health to our AIs. Working out bugs and such.

---
+++ FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GUN;
+
+public class AIDamage : MonoBehaviour {
+
+	//private Gun gun;
+	public float Health = 100.0f;
+	//public int bonusDamage = 2;
+
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Health);
+	}
+
+	void OnTriggerEnter(Collider other){
+			if (Health >= 0) {
+				Health -= 10;
+			} else {
+				Health = 0;
+			}
+		}
+}
