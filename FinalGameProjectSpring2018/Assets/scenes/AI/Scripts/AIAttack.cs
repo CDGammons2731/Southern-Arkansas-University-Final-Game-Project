f0304caaf4651ffdada59db48ff00ec930b3f84f
DESKTOP-RKUIVOR\TheAuthor <Gammons2731@muleriders.saumag.edu>
2018-03-05 23:24:13 -0600

    Made fixes to the AI damage, all the guns now seem to consistently know what its damage is to the robot. I also have the template set up for the base animations of the robot and made a fix to where the robots aren't quite so shaky anymore.

---
+++ FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAttack : MonoBehaviour {
+
+	//public Animation Meleeanim;
+	//public Animation Shootanim;
+	public GameObject ShootableObject;
+	public Transform plyr;
+	//public int Shootrange;
+	//public int Meleerange;
+	public int LookRange = 5;
+
+
+	// Use this for initialization
+	void Start () {
+		plyr = GameObject.FindGameObjectWithTag("player").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(plyr.position, gameObject.transform.position) <= LookRange){
+			gameObject.transform.LookAt (plyr);
+		}
+
+		//if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Shootrange && Vector3.Distance(plyr.position, gameObject.transform.position) > Meleerange){
+			//Shootanim.Play ();
+		//}
+
+		//if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Meleerange){
+			//Meleeanim.Play ();
+		//}
+	}
+
+}
