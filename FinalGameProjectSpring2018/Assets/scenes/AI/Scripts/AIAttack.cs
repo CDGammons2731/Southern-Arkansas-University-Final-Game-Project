cbebb8d5bdf98f888e2ede19683136237a138b54
DESKTOP-RKUIVOR\TheAuthor <Gammons2731@muleriders.saumag.edu>
2018-03-08 13:08:11 -0600

    more work on AI attack

--- FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIAttack.cs
+++ FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIAttack.cs
@@ -6,16 +6,19 @@ public class AIAttack : MonoBehaviour {
 
 	//public Animation Meleeanim;
 	//public Animation Shootanim;
+	public GameObject bullet;
 	public GameObject ShootableObject;
+	public Transform bulletSpawn;
 	public Transform plyr;
-	//public int Shootrange;
-	//public int Meleerange;
+	public int Shootrange;
+	public int Meleerange;
 	public int LookRange = 5;
-
+	public int bulletSpeed = 200;
 
 	// Use this for initialization
 	void Start () {
 		plyr = GameObject.FindGameObjectWithTag("player").transform;
+		bullet = GameObject.FindGameObjectWithTag ("bullet");
 	}
 
 	// Update is called once per frame
@@ -24,13 +27,15 @@ public class AIAttack : MonoBehaviour {
 			gameObject.transform.LookAt (plyr);
 		}
 
-		//if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Shootrange && Vector3.Distance(plyr.position, gameObject.transform.position) > Meleerange){
+		if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Shootrange && Vector3.Distance(plyr.position, gameObject.transform.position) > Meleerange){
 			//Shootanim.Play ();
-		//}
+			Instantiate(bullet, gameObject.transform.position, bulletSpawn.rotation);
+			bullet.transform.position = Vector3.MoveTowards (bullet.transform.position, plyr.position, bulletSpeed);
+		}
 
-		//if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Meleerange){
+		if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Meleerange){
 			//Meleeanim.Play ();
-		//}
+		}
 	}
 
 }
