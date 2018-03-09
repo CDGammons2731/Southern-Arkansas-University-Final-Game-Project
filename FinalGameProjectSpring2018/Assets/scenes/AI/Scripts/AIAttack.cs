455222bc3a558242a508e894a0311c9989679b7d
DESKTOP-RKUIVOR\TheAuthor <Gammons2731@muleriders.saumag.edu>
2018-03-09 07:40:47 -0600

    More AI stuff

--- FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIAttack.cs
+++ FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIAttack.cs
@@ -10,27 +10,30 @@ public class AIAttack : MonoBehaviour {
 	public GameObject ShootableObject;
 	public Transform bulletSpawn;
 	public Transform plyr;
-	public int Shootrange;
-	public int Meleerange;
+	public int Shootrange = 5;
+	public int Meleerange = 3;
 	public int LookRange = 5;
 	public int bulletSpeed = 200;
 
 	// Use this for initialization
 	void Start () {
 		plyr = GameObject.FindGameObjectWithTag("player").transform;
-		bullet = GameObject.FindGameObjectWithTag ("bullet");
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+
+
 		if(Vector3.Distance(plyr.position, gameObject.transform.position) <= LookRange){
 			gameObject.transform.LookAt (plyr);
+			Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
+			bullet.transform.position = Vector3.MoveTowards (bullet.transform.position, plyr.position, bulletSpeed);
 		}
 
-		if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Shootrange && Vector3.Distance(plyr.position, gameObject.transform.position) > Meleerange){
+		if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Shootrange){
 			//Shootanim.Play ();
-			Instantiate(bullet, gameObject.transform.position, bulletSpawn.rotation);
-			bullet.transform.position = Vector3.MoveTowards (bullet.transform.position, plyr.position, bulletSpeed);
+
 		}
 
 		if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Meleerange){
