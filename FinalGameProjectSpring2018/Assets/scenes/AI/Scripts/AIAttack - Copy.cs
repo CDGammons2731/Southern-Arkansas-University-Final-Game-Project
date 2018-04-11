7aac89fb02272a27ead11036e3d12e15aadfca84
DESKTOP-RKUIVOR\TheAuthor <Gammons2731@muleriders.saumag.edu>
2018-04-11 08:09:33 -0500

    saved data for expected error pull in Robot idle.

---
+++ FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIAttack - Copy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAttack : MonoBehaviour {
+
+	public GameObject bullet;
+	public GameObject shot;
+	public Transform bulletSpawn;
+	public Transform plyr;
+	public int Shootrange = 5;
+	public int Meleerange = 3;
+	public int LookRange = 20;
+	public int bulletSpeed = 200;
+	public static bool BoboShoot;
+
+	public int X = 0;
+
+	// Use this for initialization
+	void Start () {
+		plyr = GameObject.FindGameObjectWithTag("player").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float dist = Vector3.Distance (plyr.position, transform.position);
+		BoboShoot = AI.Escape;
+
+
+		if(BoboShoot == true && dist > 5 && dist <= LookRange){
+
+			if (X == 47) {
+				shot = (GameObject)Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation);
+				shot.GetComponent<Rigidbody>().velocity = shot.transform.forward * bulletSpeed;
+				X = 0;
+			}
+			X++;
+
+			Destroy(shot, 0.35f);
+
+		}
+
+		//if(Vector3.Distance(plyr.position, gameObject.transform.position) <= Meleerange){
+		//}
+	}
+
+}
---
+++ FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIDistanceCalculator - Copy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDistanceCalculator : MonoBehaviour {
+	public GameObject player;
+	public GameObject[] Enemies;
+	public Transform[] EnemyLocations;
+	public float[] EnemyDistances;
+	public static float ClosestEnemyDistance;
+	
+	// Update is called once per frame
+	void Update () {
+		player = GameObject.FindGameObjectWithTag ("player");
+
+		Enemies = GameObject.FindGameObjectsWithTag ("AI");
+		EnemyLocations = new Transform[Enemies.Length];
+		EnemyDistances = new float[Enemies.Length];
+
+		for (int i = 0; i < EnemyLocations.Length; i++) {
+			EnemyLocations [i] = Enemies [i].transform;
+		}
+
+		for (int i = 0; i < EnemyDistances.Length; i++) {
+			EnemyDistances [i] = Vector3.Distance (EnemyLocations [i].position, player.transform.position);
+		}
+
+		ClosestEnemyDistance = Mathf.Min (EnemyDistances);
+	}
+}
