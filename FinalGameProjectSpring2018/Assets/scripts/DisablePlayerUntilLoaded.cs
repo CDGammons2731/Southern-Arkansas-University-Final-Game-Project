67154593402cfba4959d4da452f49b13ed276df0
Walyn <walyncurtis@gmail.com>
2018-04-21 15:13:28 -0500

    Scripts on the player (such as movement) are disabled until level is done loading, tweaked view distance and level generator numbers

---
+++ FinalGameProjectSpring2018/Assets/scripts/DisablePlayerUntilLoaded.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisablePlayerUntilLoaded : MonoBehaviour {
+
+	MonoBehaviour[] scripts;
+	LevelGenerator gen;
+	bool disabled = false;
+	void Start () {
+		scripts = GetComponentsInChildren<MonoBehaviour>();
+		gen = GameObject.FindGameObjectWithTag("LevelGenerator").GetComponent<LevelGenerator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gen != null) {
+			if (gen.loaded) {
+				foreach (MonoBehaviour script in scripts) {
+					if (script != this) {
+						script.enabled = true;
+					}
+				}
+				Destroy(this);
+			} else {
+				if (!disabled) {
+					foreach (MonoBehaviour script in scripts) {
+						if (script != this) {
+							script.enabled = false;
+						}
+					}
+					disabled = true;
+				}
+			}
+		}
+	}
+}
