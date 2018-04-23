bece8bf081aa875ae1bea06d17858af4bc34b018
DESKTOP-RKUIVOR\TheAuthor <Gammons2731@muleriders.saumag.edu>
2018-04-22 21:11:18 -0500

    Added Robot shoot sound effect.

--- FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIAttack.cs
+++ FinalGameProjectSpring2018/Assets/scenes/AI/Scripts/AIAttack.cs
@@ -9,13 +9,14 @@ public class AIAttack : MonoBehaviour {
 	public Transform bulletSpawn;
 	public Transform plyr;
 	public Transform EnemyShotLookat;
+	public static bool BoboEscape;
 	public int Shootrange = 5;
 	public int Meleerange = 3;
 	public int LookRange = 20;
 	public int bulletSpeed = 200;
 	public float BobofireRate = 0.8f;
 	public float BobonextFire;
-	public static bool BoboShoot;
+	public AudioSource m_MyAudioSource;
 
 	public int X = 0;
 
@@ -23,24 +24,25 @@ public class AIAttack : MonoBehaviour {
 	void Start () {
 		plyr = GameObject.FindGameObjectWithTag("player").transform;
 		EnemyShotLookat = GameObject.FindGameObjectWithTag("Player").transform;
+		m_MyAudioSource = GetComponent<AudioSource> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		BoboShoot = AI.Escape;
-		if(BoboShoot == true && Time.time > BobonextFire){
-			if (Vector3.Distance (gameObject.transform.position, plyr.transform.position) > 3) {
-				BobonextFire = Time.time + BobofireRate;
-				shot = (GameObject)Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation);
-				shot.transform.LookAt (EnemyShotLookat.position);
-				shot.GetComponent<Rigidbody> ().velocity = shot.transform.forward * bulletSpeed;
-				Destroy (shot, 0.8f);
+
+		BoboEscape = AI.Escape;
+
+		if(Time.time > BobonextFire){
+			if (BoboEscape = true) {
+				if (Vector3.Distance (gameObject.transform.position, plyr.transform.position) > 3 && Vector3.Distance (gameObject.transform.position, plyr.transform.position) < 20) {
+					m_MyAudioSource.Play ();
+					BobonextFire = Time.time + BobofireRate;
+					shot = (GameObject)Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation);
+					shot.transform.LookAt (EnemyShotLookat.position);
+					shot.GetComponent<Rigidbody> ().velocity = shot.transform.forward * bulletSpeed;
+					Destroy (shot, 0.8f);
+				}
 			}
 		}
 	}
-
-	void Shoot(){
-		
-	}
-
 }
