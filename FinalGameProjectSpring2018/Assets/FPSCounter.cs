ba4afae42fd05717af32c60631ddcb0bab4010e5
HOME\WLCurtis9037 <walyncurtis@gmail.com>
2018-04-24 15:58:34 -0500

    FPS counter added

---
+++ FinalGameProjectSpring2018/Assets/FPSCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+
+	Text txt;
+	// Use this for initialization
+	void Start () {
+		txt = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txt.text = (1 / Time.deltaTime).ToString();
+	}
+}
