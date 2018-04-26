2c022dad15e973419e225b2f76736d586dd162c9
MelisaJ <MAJohnson4683@muleriders.saumag.edu>
2018-04-26 16:16:16 -0500

    Endings should work now!!!

--- FinalGameProjectSpring2018/Assets/scripts/LevelExit.cs
+++ FinalGameProjectSpring2018/Assets/scripts/LevelExit.cs
@@ -30,25 +30,13 @@ public class LevelExit : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("player")) {
-            
+
+
                 numLvls++;
                 SceneManager.LoadScene("main", LoadSceneMode.Single);
 
-            if(numLvls>=6){
-                hudObj.youEnd = true;
-                if (playerObj.Evidence <= 9)
-                {
-                    hudObj.endingsNum = 0;
-                }
-                else if (playerObj.Evidence >= 10 && playerObj.Evidence <= 14)
-                {
-                    hudObj.endingsNum = 1;
-                }
-                else if (playerObj.Evidence >= 15)
-                {
-                    hudObj.endingsNum = 2;
-                } 
-            }
+
+
 
 		}
 	}
