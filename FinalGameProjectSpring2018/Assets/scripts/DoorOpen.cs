a99a3a9e2146788d66ad259c0d62de2db85fd637
HOME\AVLinnear5534 <aaronlinnear@gmail.com>
2018-04-26 23:45:36 -0500

    Added a door sound

--- FinalGameProjectSpring2018/Assets/scripts/DoorOpen.cs
+++ FinalGameProjectSpring2018/Assets/scripts/DoorOpen.cs
@@ -7,9 +7,15 @@ public class DoorOpen : MonoBehaviour {
 	public float openDist = 5f;
 	public float openSpeed = 1f;
 	public float openHeight = 2f;
-	Vector3 origPos;
+    //Adding this
+    public AudioSource Sound;
+    public AudioClip door_sound;
+
+
+    Vector3 origPos;
 	void Start() {
 		origPos = transform.position;
+        Sound = GetComponent<AudioSource>();
 	}
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -18,6 +24,9 @@ public class DoorOpen : MonoBehaviour {
 		}
 		if (plr != null && Vector3.Distance (new Vector3 (plr.transform.position.x, 0, plr.transform.position.z), new Vector3 (transform.position.x, 0, transform.position.z)) < openDist) {
 			transform.position = Vector3.MoveTowards (transform.position, origPos + new Vector3 (0, openHeight, 0), openSpeed * Time.deltaTime);
+            if (door_sound != null) {
+                Sound.PlayOneShot(door_sound, 0.35f);
+            }
 		} else {
 			transform.position = Vector3.MoveTowards (transform.position, origPos, openSpeed * Time.deltaTime);
 		}
